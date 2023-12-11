dbf633b95540795e2911d0c78768607e8a51b1ad
Philippe-Arnaud Hiroux <12002903@student.pxl.be>
2023-12-11 10:50:39 +0100

    Day_2

--- 2017/Day_2.py
+++ 2017/Day_2.py
@@ -19,5 +19,27 @@ def part_1(file_Path):
 
     print(result)
 
+def part_2(file_Path):
+    with open(file_Path, 'r') as file:
+        input = [line.split("\t") for line in file]
+
+    input = [[element.rstrip('\n') for element in sublist] for sublist in input]
+    input = [[int(element) for element in sublist] for sublist in input]
+    
+    result = 0
+
+    for sublist in input:
+        length_Sublist = len(sublist)
+        for i in range(length_Sublist):
+            for j in range(i + 1, length_Sublist):
+                if sublist[i] % sublist[j] == 0 or sublist[j] % sublist[i] == 0:
+                    if sublist[i] > sublist[j]:
+                        result += sublist[i] / sublist[j]
+                    else:
+                        result += sublist[j] / sublist[i]
+    
+    print(result)
 
 part_1(file_Name)
+part_2(file_Name)
+
